cd6ecdfba0dea46c20696d478ee45a9d753c6bae
sd7776 <sdebnath600@gmail.com>
2020-01-07 14:33:55 -0500

    End of Section 12

--- DatingHub.API/Dtos/UserForRegisterDto.cs
+++ DatingHub.API/Dtos/UserForRegisterDto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DatingHub.API.Dtos
@@ -10,5 +11,29 @@ namespace DatingHub.API.Dtos
         [Required]
         [StringLength(8, MinimumLength = 4, ErrorMessage = "The password must be at least between 4 to 8 characters.")]
         public string Password { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string KnownAs { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
+        
     }
 }
