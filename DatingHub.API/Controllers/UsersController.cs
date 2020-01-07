631426118efde071f43d6f64b68b90e7ffd913de
sd7776 <sdebnath600@gmail.com>
2020-01-07 14:56:48 -0500

    End of Section 13

--- DatingHub.API/Controllers/UsersController.cs
+++ DatingHub.API/Controllers/UsersController.cs
@@ -5,11 +5,13 @@ using System.Threading.Tasks;
 using AutoMapper;
 using DatingHub.API.Data;
 using DatingHub.API.Dtos;
+using DatingHub.API.Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingHub.API.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [Authorize]
     [Route("api/[controller]")]
     [ApiController]
--- DatingHub.API/Startup.cs
+++ DatingHub.API/Startup.cs
@@ -51,6 +51,7 @@ namespace DatingHub.API
                         ValidateAudience = false
                     };
                 });
+            services.AddScoped<LogUserActivity>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
